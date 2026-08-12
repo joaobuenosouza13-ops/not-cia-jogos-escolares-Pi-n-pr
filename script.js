a23ff44448c57e3b939673eda19814eff7a5629c
joaobuenosouza13-ops <joao.bueno.souza13@escola.pr.gov.br>
2026-08-12 11:02:44 -0300

    commit

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const botao = document.getElementById("botao");
+const mensagem = document.getElementById("mensagem");
 
+botao.addEventListener("click", function () {
+    mensagem.textContent = "O esporte também é uma forma de aprender, competir e fazer novas amizades!";
+});
